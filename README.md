c6b7bbfb9e2d72376343b1c0cbc254a88d7a9f4b
Neal Dixon <neal@Neals-MacBook-Pro-16.local>
2020-08-16 21:01:32 -0700

    Updating and adding files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # UFOs
+
+## Challenge
+This project builds from the natural point where the module left off. The module work brought the site **The Truth Is Out There** to the point where it could take a single date and filter the JavaScript file containing the UFO sightings and display them in the HTML table. From that point, the challenge was to add additional filters, including 
+
+## Technologies Used
+There are two main technologies underpinning this project, one for the frontend and one on the backend. The front end is build on HTML and associated technologies. The first of these associated technologies is CSS, which almost invariably accompanies HTML sites. The use of bootstrap allows the CSS file to be rather spartan--only a background color and a background image are defined, whereas bootstrap take the lions share of setting a dynamic layout and styling. 
+
+The backend is powered by JavaScript. The information regarding UFO sightings is stored as a list of dictionaries in a JS file while the code to return the filtered data is coded in its own files. Accompanying JS, the Data-Driven Documents, or D3, allows for interactivity between user inputs and the JavaScript filters. 
+
+These technologies work together as such. Then webpage structure is created in HTML and dressed up using CSS and bootstrap. Users may enter filters and D3 is the connection that hands those inputs into the JS function and the return is displayed in the webpage as an HTML table.
+
+## Further Development
+While the webpage is able to take user inputs and create filters based on the exact inputs, such pure interaction does not work very well in the real world. Often, what users input into a system will not be the pristine condition that this program requires. The most basic example of this limitation is if a user correctly inputs a value into a field where there is no match, the table will return empty. A basic fix for this problem would be an encapsulating if-else statement that will return a message.
+
+However, it's entirely possible that a user will simply enter a value incorrectly. A live lookup and matching of values would help ensure that users cannot enter incorrect values. 
+
+Finally, there are usability issues inherent with this program. It is usually not practical or particularly useful to look up just one date. A relatively simple solution would be to have a date range and to filter results based on being greater-than or equal to the lower limit and less-than or equal to the upper limit. Another usability issue with the program is that typing in values for the filters is not the most practical operation for all types. Date might be better entered via a pop-up calendar. States could be a list. But the most glaring and perhaps unusual filter is that for shape. The JS data should be cleared up and generalized into just a few different kinds of shapes that can be represented in a list--after all, how different is a rhombus from a diamond?
--- static/js/app.js
+++ static/js/app.js
@@ -1,83 +1,87 @@
 // import the data from data.js
 const tableData = data;
+
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// My typing this in
 function buildTable(data) {
-    // Clear existing data in table
-    tbody.html(""); //tbody refers to table
-    data.forEach((dataRow) => {
-        let row = tbody.append("tr");
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-        });
-        ]
-    });
-    // Loop through each object in data and append a row and cells for each value in the row
-    data.forEach((dataRow) => {
-        // Append a row to the table body
-        let row = tbody.append("tr");
+  // Clear out any existing data in table
+  tbody.html("");
 
-        // Loop through each field in the dataRow and add each value as a cell (td)
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-            cell.text(val);
-        });
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+    });
+  });
+}
+
+// Create variable for all filters
+var filters = {};
+
+// This function will replace your handleClick function
+function updateFilters() {
+  let date = d3.select("#datetime").property("value").toLowerCase();
+  let city = d3.select("#city").property("value").toLowerCase();
+  let state = d3.select("#state").property("value").toLowerCase();
+  let country = d3.select("#country").property("value").toLowerCase();
+  let shape = d3.select("#shape").property("value").toLowerCase();
+
+  // ##### Skipping placing this into a dictionary
+  // Save the element, value, and id of the filter that was changed
+  // #####
+
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
+  // ##### Doing so within each variable
+
+  if(date) { filters['datetime'] = date; } 
+  else { delete filters['datetime']; }
+
+  if(city) { filters['city'] = city; } 
+  else { delete filters['city']; }
+
+  if(state) { filters['state'] = state; } 
+  else { delete filters['state']; }
+
+  if(country) { filters['country'] = country; } 
+  else { delete filters['country']; }
+
+  if(shape) { filters['shape'] = shape; } 
+  else { delete filters['shape']; }
+
+  // Call function to apply all filters and rebuild the table
+  filterTable();
+}
+
+function filterTable() {
+
+  // Set the filteredData to the tableData
+  let filteredData = tableData;
+
+  // Loop through all of the filters and keep any data that
+  // matches the filter values
+  if(filters) {
+    Object.keys(filters).forEach(function (key){
+        filteredData = filteredData.filter(row => row[key] === filters[key]);
     });
 }
 
-// function buildTable(data) {
-//     // First, clear out any existing data
-//     tbody.html("");
-  
-//     // Next, loop through each object in the data
-//     // and append a row and cells for each value in the row
-//     data.forEach((dataRow) => {
-//       // Append a row to the table body
-//       let row = tbody.append("tr");
-  
-//       // Loop through each field in the dataRow and add
-//       // each value as a table cell (td)
-//       Object.values(dataRow).forEach((val) => {
-//         let cell = row.append("td");
-//         cell.text(val);
-//         }
-//       );
-//     });
-//   }
-
-// My typing this in
-function handleClick() {
-    let date = d3.select("#datetime").property("value");
-    let filterdData = tableData;
-    if (date) {
-        filteredData = filteredData.filter(row => row.datetime === date);
-    };
-    buildTable(filteredData);
+  // Finally, rebuild the table using the filtered Data
+  buildTable(filteredData);
 }
 
-// function handleClick() {
-//     // Grab the datetime value from the filter
-//     let date = d3.select("#datetime").property("value");
-//     let filteredData = tableData;
-    
-//      // Check to see if a date was entered and filter the
-//     // data using that date.
-//     if (date) {
-//       // Apply `filter` to the table data to only keep the
-//       // rows where the `datetime` value matches the filter value
-//       filteredData = filteredData.filter(row => row.datetime === date);
-//     };
-    
-//      // Rebuild the table using the filtered data
-//     // @NOTE: If no date was entered, then filteredData will
-//     // just be the original tableData.
-//     buildTable(filteredData);
-//   };
-
-// Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click", handleClick);
+// Attach an event to listen for changes to each filter
+// Hint: You'll need to select the event and what it is listening for within each set of parenthesis
+d3.selectAll("#filter-btn").on("click", filterTable);
+//d3.select('#clear-btn').on('click', clearFilters);
 
 // Build the table when the page loads
 buildTable(tableData);
